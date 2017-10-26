d3f56e263240cf6fb755bdca79a53402faa85451
Florin Ionce <ionce.florin@gmail.com>
2017-10-27 01:02:08 +0300

    Fix indentation and add project description

--- extension.js
+++ extension.js
@@ -1,33 +1,39 @@
 const vscode = require('vscode');
-const ncp = require('copy-paste')
+const ncp = require('copy-paste');
 
 function activate(context) {
     var filePath = "";
-    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    var statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
     statusBarItem.command = 'extension.copyCurrentPath';
     statusBarItem.tooltip = 'Click to copy path';
-    statusBarItem.text = "$(clippy) Pathify"
+    statusBarItem.text = "$(clippy) Pathify";
     statusBarItem.show();
+
     var disposable = vscode.commands.registerCommand('extension.copyCurrentPath', function () {
         var editor = vscode.window.activeTextEditor;
-            if (!editor) {
-                vscode.window.showInformationMessage("You need to have a file opened.");
-                return;
-            }
-            filePath = editor.document.fileName.replace(vscode.workspace.rootPath + '/', '');
-            console.log(filePath)
-            statusBarItem.text = "$(checklist)   "+filePath;
-            vscode.window.showInformationMessage("Path "+filePath + " copied to clipboard");
-            ncp.copy(filePath, function () {});
-            setTimeout(function(){
-                statusBarItem.text = "$(clippy) Pathify";
-            },5000)
+
+        if (!editor) {
+            vscode.window.showInformationMessage("You need to have a file opened.");
+            return;
+        }
+
+        filePath = editor.document.fileName.replace(vscode.workspace.rootPath + '/', '');
+        statusBarItem.text = "$(checklist)   "+filePath;
+        vscode.window.showInformationMessage("Path "+filePath + " copied to clipboard");
+
+        ncp.copy(filePath, function () {});
+
+        setTimeout(function(){
+            statusBarItem.text = "$(clippy) Pathify";
+        }, 5000);
+
         });
+
         context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
+
 exports.deactivate = deactivate;
